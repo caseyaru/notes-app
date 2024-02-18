3cf3ab9d9102013dfe36f4043eb5b9abefa060da
Casey <arramine@mail.ru>
2024-02-18 21:12:48 +0300

    popup styles

--- frontend/components/cardPopup.tsx
+++ frontend/components/cardPopup.tsx
@@ -1,16 +1,26 @@
 import { Note } from "@/types/note";
 import React from "react";
-import { IconPencil, IconTrashFilled } from '@tabler/icons-react';
+import { IconPencil, IconTrashFilled, IconX } from "@tabler/icons-react";
 
 const CardPopup = ({ card }: { card: Note }) => {
   return (
     <div className="fixed top-0 left-0 h-full w-full flex items-center justify-center z-10 bg-shadow">
-      <div className="w-3/4 h-3/4 bg-white m-auto py-4 px-5">
-        <div className="flex flex-row gap-3 justify-end">
-          <IconPencil width="35" height="35" />
-          <IconTrashFilled width="35" height="35" />
+      <div className="w-3/4 h-3/4 bg-white mx-auto py-4 px-5 rounded-md relative">
+        <div className="py-4 px-5 overflow-auto h-[90%]">
+          <h2 className="text-4xl font-bold">{card.header}</h2>
+          <p className="text-xl mt-2">{card.data}</p>
         </div>
-        {card.data}
+        <div className="flex flex-row gap-3 justify-end py-1">
+          <button>
+            <IconPencil width="35" height="35" />
+          </button>
+          <button>
+            <IconTrashFilled width="35" height="35" />
+          </button>
+        </div>
+        <button className="absolute top-0 -right-12">
+          <IconX width="45" height="55" className="text-white" />
+        </button>
       </div>
     </div>
   );
