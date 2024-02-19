be04c42ee70e7cbb8d847182947e3f1b942026ba
Casey <arramine@mail.ru>
2024-02-19 21:37:28 +0300

    closing state

--- frontend/components/cardPopup.tsx
+++ frontend/components/cardPopup.tsx
@@ -1,8 +1,18 @@
 import { Note } from "@/types/note";
 import React from "react";
+import { setShowPopup } from "@/store/popupSlice";
 import { IconPencil, IconTrashFilled, IconX } from "@tabler/icons-react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPopups } from "@/store";
 
 const CardPopup = ({ card }: { card: Note }) => {
+  const { showPopup } = useSelector(getPopups);
+  const dispatch = useDispatch();
+  
+  const handleClose = () => {
+    dispatch(setShowPopup(false));
+  };
+
   return (
     <div className="fixed top-0 left-0 h-full w-full flex items-center justify-center z-10 bg-shadow">
       <div className="w-3/4 h-3/4 bg-white mx-auto py-4 px-5 rounded-md relative">
@@ -18,7 +28,7 @@ const CardPopup = ({ card }: { card: Note }) => {
             <IconTrashFilled width="35" height="35" />
           </button>
         </div>
-        <button className="absolute top-0 -right-12">
+        <button className="absolute top-0 -right-12" onClick={handleClose}>
           <IconX width="45" height="55" className="text-white" />
         </button>
       </div>
